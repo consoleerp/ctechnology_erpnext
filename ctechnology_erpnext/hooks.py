da0624b790b27e635ea98dbd67eec376e37fe22d
Console Admin <faztp12@gmail.com>
2017-02-02 10:17:26 +0300

    has qty-fixture update

--- ctechnology_erpnext/hooks.py
+++ ctechnology_erpnext/hooks.py
@@ -120,7 +120,7 @@ app_license = "MIT"
 # }
 
 fixtures = [
-	{"dt" : "Custom Field", "filters" : [["name", "in", ['Mode of Payment-consoleerp_territory', 'Sales Invoice Item-consoleerp_cost']]]}, 
+	{"dt" : "Custom Field", "filters" : [["name", "in", ['Mode of Payment-consoleerp_territory', 'Sales Invoice Item-consoleerp_cost', 'Sales Invoice-consoleerp_hasqty']]]}, 
 	"Custom Script", 
 	{"dt" : "Print Format", "filters" : [["name", "in", ['Control Technology Sales Invoice']]]}, 
 	{"dt" : "Property Setter", "filters" : [["name", "in", [' Sales Invoice-default_print_format']]]}]
