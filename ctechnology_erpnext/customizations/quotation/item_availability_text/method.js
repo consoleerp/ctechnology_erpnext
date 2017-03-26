26540ec5254eedcfea2d453e83dd3f77a3a886cb
Console Admin <faztp12@gmail.com>
2017-03-26 10:29:16 +0300

    bugs

--- ctechnology_erpnext/customizations/quotation/item_availability_text/method.js
+++ ctechnology_erpnext/customizations/quotation/item_availability_text/method.js
@@ -3,13 +3,13 @@ function update_qty_availability(frm, cdt, cdn) {
 	var child_doc = locals[cdt][cdn];
 	
 	if (!child_doc.consoleerp_item_total_qty)
-		return;
+		child_doc.consoleerp_item_total_qty = 0;
 	
 	// if null, not zero
 	if (child_doc.qty == null)
 		child_doc.qty = 1
 		
-	if (child_doc.consoleerp_item_total_qty > child_doc.qty) // if tot qty > req. qty -- available		
+	if (child_doc.consoleerp_item_total_qty >= child_doc.qty) // if tot qty > req. qty -- available		
 		frappe.model.set_value(cdt, cdn, "consoleerp_item_availability", "Available");
 	else if (!child_doc.consoleerp_item_total_qty || child_doc.consoleerp_item_total_qty <= 0) // if qty not avaialbe
 		frappe.model.set_value(cdt, cdn, "consoleerp_item_availability", "Not Available");
